34cabd5fc5eb03076a2b776a06c2c7adf29d4982
GYCHEN <gy.chen@gms.nutc.edu.tw>
2017-12-11 21:23:58 +0800

    add search

--- hiddenyoutu_server/web.py
+++ hiddenyoutu_server/web.py
@@ -11,8 +11,24 @@ from hiddenyoutu_server import youtube
 app = Flask(__name__)
 
 
-@app.route('/related_videos/<video_id>')
+@app.route('/list/<q>')
+def search(q):
+    api_key = request.args.get('key', None)
+    page_token = request.args.get('pageToken', None)
+    videos = youtube.search(
+        q,
+        page_token=page_token,
+        api_key=api_key
+    )
+    return jsonify(videos)
+
+
+@app.route('/list/related_videos/<video_id>')
 def search_related_video(video_id):
-    next_page_token = request.args.get('next_page_token', None)
-    related_videos = youtube.search_related_videos(video_id, next_page_token=next_page_token)
+    api_key = request.args.get('key', None)
+    page_token = request.args.get('pageToken', None)
+    related_videos = youtube.search_related_videos(
+        video_id,
+        page_token=page_token,
+        api_key=api_key)
     return jsonify(related_videos)
--- hiddenyoutu_server/youtube.py
+++ hiddenyoutu_server/youtube.py
@@ -9,8 +9,13 @@ import os
 import requests
 from hiddenyoutu_server import setting
 
+__ALL__ = [
+    'search_related_videos',
+    'search']
+
 # see https://developers.google.com/youtube/v3/docs/search/list
 LIST_URL = 'https://www.googleapis.com/youtube/v3/search?'
+LIST_QS_KEY_Q = 'q'
 LIST_QS_KEY_TYPE = 'type'
 LIST_QS_KEY_RELATED_VIDEO_ID = 'relatedToVideoId'
 LIST_QS_KEY_PART = 'part'
@@ -19,16 +24,37 @@ LIST_QS_KEY_PAGE_TOKEN = 'pageToken'
 
 
 # TODO add doc string
-def search_related_videos(video_id, next_page_token=None, api_key=None):
+def search_related_videos(video_id, page_token=None, api_key=None):
+    api_key = api_key if api_key else init_api_key()
     if api_key is None:
-        api_key = os.environ.get('YOUTUBE_API_KEY', setting.YOUTUBE_API_KEY)
+        raise ValueError("Please provide API key. See: https://console.developers.google.com/")
     params = {
         LIST_QS_KEY_TYPE: 'video',
         LIST_QS_KEY_RELATED_VIDEO_ID: video_id,
         LIST_QS_KEY_PART: 'snippet',
         LIST_QS_KEY_API_KEY: api_key,
-        LIST_QS_KEY_PAGE_TOKEN: next_page_token
+        LIST_QS_KEY_PAGE_TOKEN: page_token
     }
     r = requests.get(LIST_URL, params=params)
     # TODO check response status
     return r.json()
+
+
+def search(q, page_token=None, api_key=None):
+    api_key = api_key if api_key else init_api_key()
+    if api_key is None:
+        raise ValueError("Please provide API key. See: https://console.developers.google.com/")
+    params = {
+        LIST_QS_KEY_Q: q,
+        LIST_QS_KEY_TYPE: 'video',
+        LIST_QS_KEY_PART: 'snippet',
+        LIST_QS_KEY_API_KEY: api_key,
+        LIST_QS_KEY_PAGE_TOKEN: page_token
+    }
+    r = requests.get(LIST_URL, params=params)
+    # TODO check response status
+    return r.json()
+
+
+def init_api_key():
+    return os.environ.get('YOUTUBE_API_KEY', setting.YOUTUBE_API_KEY)
